e480acc38ccf5a90524c80801925c8ef11ea54fc
BugHunter-X <askzenai@gmail.com>
2025-02-05 15:18:05 +0100

    Add app/page.js file

---
+++ app/page.js
@@ -0,0 +1,18 @@
+```javascript
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div>Hello</div>
+  );
+}
+```
+
+```javascript
+// app/page.js
+export default function Home() {
+  return (
+    <div>Hello</div>
+  );
+}
+```
